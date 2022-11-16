5fe6ce9fcde86ce8455e2a3021bb96b2c3fdab11
el-antiguo <codomodragon2@gmail.com>
2022-11-16 18:06:58 -0400

    [php] crear controlador para login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\Auth\CreateUser;
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+
+        return redirect("login");
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function processRegistration(
+        CreateUser $createUser,
+        Request $request
+    ) {
+        $createUser->create($request->all());
+
+        return redirect("dashboard");
+    }
+
+    public function signOut()
+    {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
