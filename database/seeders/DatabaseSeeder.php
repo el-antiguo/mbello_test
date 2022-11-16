9cf97f94de6df1d620e4bb879c2519e0ef013cf3
el-antiguo <codomodragon2@gmail.com>
2022-11-16 18:16:55 -0400

    [php] agregar sedder inicial

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Producto;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->createMany([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@test.com',
+                'role' => User::ADMIN_ROLE,
+            ],
+            [
+                'name' => 'user1',
+                'email' => 'user1@test.com',
+                'role' => User::USER_ROLE,
+            ],
+            [
+                'name' => 'user2',
+                'email' => 'user2@test.com',
+                'role' => User::USER_ROLE,
+            ],
+        ]);
+        Producto::factory()->createMany([
+            [
+                'nombre' => 'Producto 1',
+                'precio' => 123.45,
+                'impuesto' => 5,
+            ],
+            [
+                'nombre' => 'Producto 2',
+                'precio' => 45.65,
+                'impuesto' => 15,
+            ],
+            [
+                'nombre' => 'Producto 3',
+                'precio' => 39.73,
+                'impuesto' => 12,
+            ],
+            [
+                'nombre' => 'Producto 4',
+                'precio' => 250.00,
+                'impuesto' => 8,
+            ],
+            [
+                'nombre' => 'Producto 5',
+                'precio' => 59.35,
+                'impuesto' => 10,
+            ],
+        ]);
+
     }
 }
